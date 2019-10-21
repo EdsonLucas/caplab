9e54efdfc9e77008c935b8f6eda41f6bafb05d82
Edson Lucas <contato@edsonlucas.com.br>
2019-10-20 23:35:33 -0300

    build files

---
+++ precache-manifest.f49131c0e4a95a773bf73549187ab0f6.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0311e50349d31d60040aa79bc49e0d64",
+    "url": "/index.html"
+  },
+  {
+    "revision": "647ba77aed98e5a2071a",
+    "url": "/static/js/2.bdb6d3cd.chunk.js"
+  },
+  {
+    "revision": "ed57dc5631dec889cdbd",
+    "url": "/static/js/main.03ab4535.chunk.js"
+  },
+  {
+    "revision": "a66d1e84c09c92f1ead2",
+    "url": "/static/js/runtime-main.ae5d1a39.js"
+  },
+  {
+    "revision": "d4475b240be759f257bc919fed84af2b",
+    "url": "/static/media/avatar-default.d4475b24.svg"
+  },
+  {
+    "revision": "7646e8dfac8977b8f122303a13c46a37",
+    "url": "/static/media/bg-header-overview.7646e8df.png"
+  },
+  {
+    "revision": "7fe59bd9c4f3dec43114a5f1616f3913",
+    "url": "/static/media/capitual-brand.7fe59bd9.svg"
+  },
+  {
+    "revision": "61e96a652114a62854006f5b3aae8f58",
+    "url": "/static/media/email-gray.61e96a65.svg"
+  },
+  {
+    "revision": "922464a1f1732524833fd8a5fdddf85c",
+    "url": "/static/media/icon-evoucher-gray.922464a1.svg"
+  },
+  {
+    "revision": "25a07d12811eea8c2e235e8e2939ad47",
+    "url": "/static/media/icon-invoice-gray.25a07d12.svg"
+  },
+  {
+    "revision": "d737fa9beeb09c46f16e217f735e94a7",
+    "url": "/static/media/icon-payment-gray.d737fa9b.svg"
+  },
+  {
+    "revision": "fb14559c4707abb1df4c1a61dfef0165",
+    "url": "/static/media/icon-wallet-gray.fb14559c.svg"
+  },
+  {
+    "revision": "7a7b630b08f33068ea8283e860eb775b",
+    "url": "/static/media/phone-gray.7a7b630b.svg"
+  },
+  {
+    "revision": "f0c30647ec8a5c0a900f156ce9262490",
+    "url": "/static/media/profile-gray.f0c30647.svg"
+  },
+  {
+    "revision": "7346faf06b6e591f101244f6051ba442",
+    "url": "/static/media/security-gray.7346faf0.svg"
+  },
+  {
+    "revision": "770e5612eed5d90f37f4d19721902451",
+    "url": "/static/media/user-white.770e5612.svg"
+  },
+  {
+    "revision": "85d7becd87e9b106a557427a4f1f7c69",
+    "url": "/static/media/verified.85d7becd.svg"
+  }
+]);
